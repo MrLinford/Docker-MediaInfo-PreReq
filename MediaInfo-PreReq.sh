83cae141d13402197be148076bba49d01414cbdf
Craig Linford <clinford86@gmail.com>
2023-05-07 08:03:04 +0100

    Added Comments

--- MediaInfo-PreReq.sh
+++ MediaInfo-PreReq.sh
@@ -1,26 +1,33 @@
 #! /bin/bash
 
+# Make sure current packages are updated
 apt update
 
+# Install MediaInfoCLI 
 apt-get -y install mediainfo
 
+# Import MKVToolNix GPG key 
 curl -o /usr/share/keyrings/gpg-pub-moritzbunkus.gpg https://mkvtoolnix.download/gpg-pub-moritzbunkus.gpg
 
+# Add MKVToolNix repositorie
 cd /etc/apt/sources.list.d
-
 touch mkvtoolnix.download.list
-
 cat > mkvtoolnix.download.list << EOF
-deb [signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/ubuntu/ jammy main
-deb-src [signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/ubuntu/ jammy main
+deb [arch=amd64 signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/ubuntu/ jammy main
+deb-src [arch=amd64 signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/ubuntu/ jammy main
 EOF
 
+# Make sure current packages are updated
 apt update
 
+# Install MKVToolNix Portable
 apt -y install mkvtoolnix=70.0.0-0~ubuntu2204bunkus01 mkvtoolnix-gui=70.0.0-0~ubuntu2204bunkus01
 
+# Download BifTool Executables
 curl -Lo /tmp/biftool_linux.zip https://github.com/rokudev/samples/raw/master/utilities/bif%20tool/biftool_linux.zip
 
+# Install unzip
 apt-get -y install unzip
 
+# Extract BifTool files to /bin/
 unzip /tmp/biftool_linux.zip -d /bin
